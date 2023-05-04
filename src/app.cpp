49961c5920362e79a2a6d5bba2a0efb40de9b5e4
Akane <128232444+mrakane7@users.noreply.github.com>
2023-05-04 19:24:08 +0200

    Update app.cpp

--- src/app.cpp
+++ src/app.cpp
@@ -1,13 +1,15 @@
 #include <app.hpp>
 
-ANXLayout::ANXLayout() : Layout::Layout() {
+ANXLayout::ANXLayout() : Layout::Layout()
+{
     this->helloText = pu::ui::elm::TextBlock::New(0, 0, "Welcome to Anime NX!");
     
     // Add the instance to the layout.
     this->Add(this->helloText);
 }
 
-void ANXApplication::OnLoad() {
+void ANXApplication::OnLoad()
+{
     this->layout = ANXLayout::New();
 
     this->LoadLayout(this->layout);
